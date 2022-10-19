e401a830ea8d8e40a581ed44c0ea3e984e6256be
LPavel98 <pavel.perez.alvarez@gmail.com>
2022-10-18 21:45:11 -0500

    movements

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -72,8 +72,8 @@ public class PlayerMovement : MonoBehaviour
         anim.SetInteger("state", (int)state);
     }
 
-    private bool IsGrounded()
-    {
-        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
-    }
+    // private bool IsGrounded()
+    // {
+    //     return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
+    // }
 }
